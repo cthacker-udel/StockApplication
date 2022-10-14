0683a22206ef5e1f0e09bcb4a18a90cd3feb1f85
cthacker-udel <cthacker@udel.edu>
2022-10-14 00:58:13 -0400

    [sendgrid-implementation] Added in email templates managed by sendgrid, look a ton better

--- src/server/common/email/templates/emailtemplates.ts
+++ src/server/common/email/templates/emailtemplates.ts
@@ -1,12 +1,5 @@
-type ForgotPasswordArguments = {
-	token: string;
-};
+const FORGOT_PASSWORD_TEMPLATE = "d-50f9e9a6a2df4e60b93f3e94672b05e2";
 
-const FORGOT_PASSWORD_TEMPLATE = ({ token }: ForgotPasswordArguments) =>
-	`<html><body><div style="font-size: .75em; margin-bottom: 5vh;">474 Stock Trader Application</div><div style="text-align: center; font-weight: bold;">Forgot Password Link</div><div style="margin-top: 10vh; text-align: center;"><a href="http://localhost:4000/api/forgot/password?token=${token}">Click here to change password</a></div></body></html>`;
-
-export const EMAIL_TEMPLATES: {
-	[key: string]: string | ((_arguments: any) => string);
-} = {
+export const EMAIL_TEMPLATES = {
 	forgotPassword: FORGOT_PASSWORD_TEMPLATE,
 };
