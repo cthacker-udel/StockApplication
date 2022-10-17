dc515af19a4143726b96a719544abbf70038113d
cthacker-udel <cthacker@udel.edu>
2022-10-17 03:01:01 -0400

    [cookie-session] Added basis for login page

---
+++ src/client/src/app/_models/User.ts
@@ -0,0 +1,4 @@
+export type User = {
+  password: string;
+  username: string;
+};
--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
