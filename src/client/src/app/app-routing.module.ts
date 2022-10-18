e035fab9b7d1925edf334adca79926169bb7f28a
maybeep <60990725+maybeep@users.noreply.github.com>
2022-10-18 17:24:18 -0400

    commit to pull from main, added import

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './_components/login.component';
 
 const routes: Routes = [
   {
