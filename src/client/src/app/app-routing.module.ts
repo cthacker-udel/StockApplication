29f7fa858a399eedea8faad0667a7f11c17bc9d2
maybeep <60990725+maybeep@users.noreply.github.com>
2022-10-18 17:24:18 -0400

    commit to pull from main, added import

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-import { LoginComponent } from './_components/login.component';
-=======
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './_components/landingpage/landingpage.component';
->>>>>>> 44aa289 ([landing_page] Added landing page)
-
 const routes: Routes = [
   {
     component: LandingPageComponent,
