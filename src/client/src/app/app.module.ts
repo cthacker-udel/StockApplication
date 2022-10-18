28f49b5f35a178ef9e09efb26c7a6ed6b4317c3c
cthacker-udel <cthacker@udel.edu>
2022-10-18 01:43:20 -0400

    [landing_page] Added full-fledged landing page with validation for sign up

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -5,14 +5,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LandingPageComponent } from './_components/landingpage/landingpage.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LandingPageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FontAwesomeModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
