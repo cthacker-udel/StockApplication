44aa28963f0cfb51f30ea86c0f906f0e94c424eb
cthacker-udel <cthacker@udel.edu>
2022-10-17 21:58:07 -0400

    [landing_page] Added landing page

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FontAwesomeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
