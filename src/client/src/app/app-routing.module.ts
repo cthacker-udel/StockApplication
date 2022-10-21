8715ffb5bdc8d4e33d24f3221d4aad1705465286
maybeep <60990725+maybeep@users.noreply.github.com>
2022-10-21 14:22:30 -0400

    adding portfolio page files and route

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './_components/landingpage/landingpage.component';
+import { PortfolioPageComponent } from './_components/portfoliopage/portfoliopage.component';
 
 const routes: Routes = [
   {
     component: LandingPageComponent,
     path: '',
     outlet: 'landing-page',
+  },
+  {
+    component: PortfolioPageComponent,
+    path: '',
+    outlet: 'portfolio-page',
   },
 ];
 
