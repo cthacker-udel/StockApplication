124ecfd361795c6c3476fe4b5a2b2ac567b74362
cthacker-udel <cthacker@udel.edu>
2022-10-21 12:36:49 -0400

    [roles-2] Added roles middleware injector

--- src/server/@types/api/index.ts
+++ src/server/@types/api/index.ts
@@ -3,3 +3,4 @@ export * from "./common";
 export * from "./user";
 export * from "./status";
 export * from "./role";
+export * from "./session";
--- src/server/@types/api/session/RedisSession.ts
+++
@@ -1,4 +0,0 @@
-export type RedisSession = {
-	sessionToken: string;
-	username: string;
-};
