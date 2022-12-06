370c7c3a3cd5c202c23740efa733cd13a7c523cd
cthacker-udel <cthacker@udel.edu>
2022-12-06 03:16:49 -0500

    [sell-improvement] Added improvements to the profile page

--- src/client/src/app/_services/user.service.ts
+++ src/client/src/app/_services/user.service.ts
@@ -3,6 +3,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { ROUTE_PREFIXES } from 'src/shared/constants/api';
 import { ConfigService } from '../config/config.service';
 import { User } from '../_models/User';
+import { UserAggregateData } from '../_models/UserAggregateData';
 
 /**
  * The user service, controls all functionality involving users
@@ -22,6 +23,12 @@ export class UserService {
   getUserDataWithUsername(username: string): Observable<User> {
     return this.httpClient.getConfig<User>(
       `${ROUTE_PREFIXES.user}data?username=${username}`
+    );
+  }
+
+  getUserAggregateData(username: string): Observable<UserAggregateData> {
+    return this.httpClient.getConfig<UserAggregateData>(
+      `${ROUTE_PREFIXES.user}aggregate?username=${username}`
     );
   }
 }
