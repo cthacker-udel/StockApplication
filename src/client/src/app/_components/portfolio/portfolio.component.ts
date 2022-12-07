f18fcf43aa10a9c45c92e9e69c23d0485408bd4a
Treah <tholley@udel.edu>
2022-12-06 23:30:06 -0500

    tons of logic changes

--- src/client/src/app/_components/portfolio/portfolio.component.ts
+++ src/client/src/app/_components/portfolio/portfolio.component.ts
@@ -1,12 +1,13 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { UserAggregateData } from 'src/app/_models/UserAggregateData';
+import { UserService } from 'src/app/_services/user.service';
 import { ConfigService } from 'src/app/config/config.service';
 import { OwnedStock } from 'src/app/_models/OwnedStock';
 import { SessionCookie } from 'src/app/_models/SessionCookie';
 import { Stock } from 'src/app/_models/Stock';
 import { User } from 'src/app/_models/User';
 import { StockAppSocketService } from 'src/app/_services/stockappsocket.service';
-import { TradingService } from 'src/app/_services/trading.service';
 import { SECRETS } from 'src/secrets';
 import { ROUTE_PREFIXES } from 'src/shared/constants/api';
 
@@ -34,6 +35,9 @@ type UserPortfolioStock = {
   styleUrls: ['./portfolio.component.css'],
 })
 export class PortfolioComponent {
+  currentUser: Partial<User>;
+  currentUserStockSymbols: string[];
+  userAggregateData: UserAggregateData;
   userOwnedStocks: MatTableDataSource<UserPortfolioStock>;
   displayLoading: boolean;
 
@@ -46,6 +50,7 @@ export class PortfolioComponent {
   ];
 
   constructor(
+    private userService: UserService,
     public configService: ConfigService,
     public stockAppSocketService: StockAppSocketService
   ) {
@@ -80,11 +85,12 @@ export class PortfolioComponent {
               );
               console.log(matchedStock);
               if (matchedStock !== undefined) {
+                const priceDiff = eachStock.price - eachStock.oldPrice;
                 const changeResult =
                   (eachStock.price - eachStock.oldPrice) * 100;
                 const gainLoss =
-                  matchedStock.amount * eachStock.price -
-                  matchedStock.amount * eachStock.oldPrice;
+                 (matchedStock.amount * eachStock.price) -
+                  (matchedStock.amount * eachStock.oldPrice);
                 ('');
                 convertedUserStocks.push({
                   balance: matchedStock.amount * eachStock.price,
@@ -93,7 +99,7 @@ export class PortfolioComponent {
                     negative: false,
                     neutral: true,
                   },
-                  change: (eachStock.price - eachStock.oldPrice) * 100,
+                  change: Number.parseFloat(((priceDiff / eachStock.oldPrice) * 100).toFixed(2)),
                   changeReaction: {
                     positive: changeResult > 0,
                     negative: changeResult < 0,
@@ -106,8 +112,8 @@ export class PortfolioComponent {
                     neutral: true,
                   },
                   gainLoss:
-                    matchedStock.amount * eachStock.price -
-                    matchedStock.amount * eachStock.oldPrice,
+                    Number.parseFloat(((matchedStock.amount * eachStock.price) -
+                    (matchedStock.amount * eachStock.oldPrice)).toFixed(2)),
                   gainLossReaction: {
                     positive: gainLoss > 0,
                     negative: gainLoss < 0,
@@ -131,20 +137,22 @@ export class PortfolioComponent {
         .subscribe((_res: Partial<Stock>) => {
           console.log('stock updated ', _res);
           const { symbol, price, oldPrice } = _res;
+          //const prevPrice = eachStock.oldPrice;
           if (price) {
             // price changed, begin computation
             const displayedStockIndex = this.userOwnedStocks.data.findIndex(
               (eachDisplayedStock: UserPortfolioStock) =>
                 eachDisplayedStock.symbol === symbol
             );
-            if (displayedStockIndex !== -1) {
+            if (displayedStockIndex !== -1 && oldPrice !== undefined) {
+              console.log(oldPrice);
               console.log('updating stock');
               const foundStock = this.userOwnedStocks.data[displayedStockIndex];
               const changeResult =
-                price - (oldPrice ?? foundStock.currentPrice);
+                Number.parseFloat((price - (oldPrice ?? foundStock.currentPrice)).toFixed(2));
               const gainLossResult =
-                foundStock.balance * foundStock.currentPrice -
-                foundStock.balance * price;
+                Number.parseFloat(((foundStock.balance * foundStock.currentPrice) -
+                (foundStock.balance * price)).toFixed(2));
               const cloneUserStockData = [...this.userOwnedStocks.data];
               const balanceDifference =
                 cloneUserStockData[displayedStockIndex].balance -
@@ -154,7 +162,7 @@ export class PortfolioComponent {
 
               cloneUserStockData[displayedStockIndex] = {
                 ...foundStock,
-                balance: foundStock.balance * price,
+                balance: foundStock.balance,
                 balanceReaction: {
                   positive: balanceDifference > 0,
                   negative: balanceDifference < 0,
@@ -184,5 +192,80 @@ export class PortfolioComponent {
           }
         });
     }
+  }
+
+  ngOnInit(): void {
+    this.userService.currentUser.subscribe((value: { user: Partial<User> }) => {
+      if (value && Object.keys(value?.user).length > 0) {
+        const { user } = value;
+        this.currentUser = user;
+      }
+    });
+    const storedUsername = localStorage.getItem(
+      SECRETS.STOCK_APP_SESSION_COOKIE_USERNAME_ID
+    );
+    if (storedUsername !== null) {
+      // found user, grab information
+      const parsedUsername = JSON.parse(storedUsername) as SessionCookie;
+      const dataRequest = this.configService.getConfig<{ user: Partial<User> }>(
+        `${ROUTE_PREFIXES.user}data?username=${parsedUsername.value}`
+      );
+      dataRequest.subscribe((result: { user: Partial<User> }) => {
+        this.currentUser = result.user;
+        if (result.user.portfolio) {
+          this.currentUserStockSymbols = result.user.portfolio.stocks.map(
+            (eachStock: OwnedStock) => eachStock.symbol
+          );
+        }
+      });
+      const aggregateRequest = this.configService.getConfig<UserAggregateData>(
+        `${ROUTE_PREFIXES.user}aggregate?username=${parsedUsername.value}`
+      );
+      aggregateRequest.subscribe((result: UserAggregateData) => {
+        this.userAggregateData = result;
+      });
+
+      const potentialProfitRequest = this.configService.getConfig<
+        Partial<UserAggregateData>
+      >(
+        `${ROUTE_PREFIXES.user}potentialProfit?username=${parsedUsername.value}`
+      );
+
+      this.stockAppSocketService
+        .getStockUpdated()
+        .subscribe((changedStock: Stock) => {
+          if (
+            this.currentUserStockSymbols &&
+            this.currentUserStockSymbols.includes(changedStock.symbol)
+          ) {
+            potentialProfitRequest.subscribe(
+              (updatedPotentialProfit: Partial<UserAggregateData>) => {
+                if (updatedPotentialProfit.totalPotentialProfit) {
+                  this.userAggregateData.totalPotentialProfit =
+                    updatedPotentialProfit.totalPotentialProfit;
+                }
+              }
+            );
+          }
+        });
+
+      this.stockAppSocketService
+        .getUserUpdated()
+        .subscribe((updatedUser: Partial<User>) => {
+          this.currentUser = { ...this.currentUser, ...updatedUser };
+          potentialProfitRequest.subscribe(
+            (updatedPotentialProfit: Partial<UserAggregateData>) => {
+              if (updatedPotentialProfit.totalPotentialProfit !== undefined) {
+                this.userAggregateData.totalPotentialProfit =
+                  updatedPotentialProfit.totalPotentialProfit;
+              }
+            }
+          );
+        });
+    }
+  }
+
+  truncateDecimal(decimal: number, amount: number) {
+    return decimal.toFixed(amount);
   }
 }
